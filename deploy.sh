1193f0593e77497e440472575be1cb5b3f6a65b0
Badruddin Kamal <bulkguy47@gmail.com>
2017-08-24 09:40:58 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /bin/bash -c "sudo scp -o StrictHostKeyChecking=no -r -i /key/process_maker.pem . ubuntu@ec2-54-252-242-51.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/"
+/bin/bash -c "sudo scp -o StrictHostKeyChecking=no -r -i /key/process_maker.pem . ubuntu@ec2-54-252-242-51.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/"

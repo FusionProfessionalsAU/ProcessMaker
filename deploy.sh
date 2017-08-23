65402899564745d83c49d73cbfa41d7bdb0b6872
Badruddin Kamal <bulkguy47@gmail.com>
2017-08-24 09:57:07 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/bash -c "scp -o StrictHostKeyChecking=no --exclude /opt/processmaker//./.git/ -r -i /key/process_maker.pem . ubuntu@ec2-54-252-242-51.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/"
+/bin/bash -c "scp -o StrictHostKeyChecking=no -r -i /key/process_maker.pem /var/lib/go-agent/pipelines/Deploy/ ubuntu@ec2-54-252-242-51.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/"

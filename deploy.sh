1cc66187a08bb4b94fd3f83d39e9e763080e02e7
unknown <bulkguy47@gmail.com>
2017-08-23 16:17:36 +1000

    test script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /var/lib/go-agent/pipelines/Deploy/
-sudo -r -i /key/process_maker.pem . ubuntu@ec2-54-252-242-51.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/
+#cd /var/lib/go-agent/pipelines/Deploy/
+#sudo -r -i /key/process_maker.pem . ubuntu@ec2-54-252-242-51.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/
+echo "hello"

b8c62d07117845395aaaf667d126a34021488992
unknown <bulkguy47@gmail.com>
2017-08-25 14:49:17 +1000

    Fix script bugs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/bash -c "ssh -o StrictHostKeyChecking=no -i /key/process_maker.pem ubuntu@ec2-54-252-163-180.ap-southeast-2.compute.amazonaws.com 'sudo chmod -R 777 /opt/processmaker/'"
-/bin/bash -c "scp -o StrictHostKeyChecking=no -r -i /key/process_maker.pem /var/lib/go-agent/pipelines/Deploy/ ubuntu@ec2-54-252-163-180.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/"
+/bin/bash -c "scp -o StrictHostKeyChecking=no -r -i /key/process_maker.pem . ubuntu@ec2-54-252-163-180.ap-southeast-2.compute.amazonaws.com:/opt/processmaker/"
